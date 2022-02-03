6b4c7634e931114a0ffaef52ff4ab3903c983cef
Nenad Ilic <ilic1993@gmail.com>
2022-02-03 12:35:22 +0100

    Initial backend setup

---
+++ backend/API/Middlewares/SwaggerAuthorizedMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace Skocko.Api.Middlewares
+{
+    public class SwaggerAuthorizedMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public IConfiguration Configuration { get; }
+        private string _letMeInPass;
+
+        public SwaggerAuthorizedMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            Configuration = configuration;
+
+            _letMeInPass = Configuration.GetValue<string>("PasswordDevOptions");
+
+            if (_letMeInPass == null)
+            {
+                // Default pass
+                _letMeInPass = "mellon";
+            }
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/swagger"))
+            {
+                if (!context.Request.Cookies.ContainsKey("pass")
+                    || context.Request.Cookies["pass"] != _letMeInPass)
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    context.Response.ContentType = "application/json; charset=utf-8";
+                    await context.Response.WriteAsync("{\"text\":\"NEЋЕ МОЋИ\"}", Encoding.UTF8);
+
+                    return;
+                }
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+}
---
+++ backend/API/Startup.cs
@@ -0,0 +1,102 @@
+﻿using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Converters;
+using Skocko.Api.Middlewares;
+
+namespace Skocko.Api
+{
+
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // needed to load configuration from appsettings.json
+            services.AddOptions();
+
+            services.AddMemoryCache();
+
+            //load general configuration from appsettings.json
+            services.Configure<IpRateLimitOptions>(Configuration.GetSection("IpRateLimiting"));
+
+            //load ip rules from appsettings.json
+            services.Configure<IpRateLimitPolicies>(Configuration.GetSection("IpRateLimitPolicies"));
+
+            // inject counter and rules stores
+            services.AddInMemoryRateLimiting();
+
+            // configuration (resolvers, counter key builders)
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+            services.AddHttpContextAccessor();
+
+            services.AddCors();
+
+            services.AddControllers()
+                    .AddNewtonsoftJson(options =>
+                        options.SerializerSettings.Converters.Add(new StringEnumConverter()));
+
+            services.AddSwaggerGenNewtonsoftSupport();
+
+            // order is vital, this *must* be called *after* AddNewtonsoftJson()
+            services.AddSwaggerGenNewtonsoftSupport();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Skočko Web API", Version = "v1" });
+            });
+
+            services.AddMvcCore(options => options.EnableEndpointRouting = false).AddRazorViewEngine();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseIpRateLimiting();
+
+            // Enable all authentications added in configure services
+            app.UseAuthentication();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMiddleware<SwaggerAuthorizedMiddleware>();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Skočko Web API v1"));
+            }
+            else
+            {
+                // TODO: Add when we go live in order to force SSL
+                // app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            // global cors policy //https://jasonwatmore.com/post/2021/05/26/net-5-api-allow-cors-requests-from-any-origin-and-with-credentials
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithExposedHeaders(new string[] { "Authorization" })
+                .SetIsOriginAllowed(origin => true) // allow any origin todo: maybe remove this later
+                .AllowCredentials()); // allow credentials
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
